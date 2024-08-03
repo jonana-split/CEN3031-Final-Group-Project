fe92af3d2bde78511f949744e494f5c142fa71f3
ssamoyeds <165983028+ssamoyeds@users.noreply.github.com>
2024-08-03 12:08:50 -0400

    Uploading the php files
    These are for adding estimate times for each ticket and for the number of hours logged on each ticket

---
+++ logHours.php
@@ -0,0 +1,34 @@
+<?php
+
+$host = "127.0.0.1";
+$user = "root";
+$password = "";
+$db="login_it";
+
+session_start();
+
+if(!isset($_SESSION["username"])){
+    header("location:login.php");
+}
+
+if(empty($_POST["loggedHours"])){
+    die("The number of hours worked is required");
+}
+
+if(empty($_POST["ticketLog"])){
+    die("The ticket you worked on is required");
+}
+
+$mysqli = require  __DIR__ . "/database.php";
+$data=mysqli_connect($host,$user,$password,$db);
+
+$sql = "UPDATE tickets SET logged_hours=$_POST[loggedHours] WHERE id=$_POST[ticketLog]";
+
+if ($data->query($sql) === TRUE) {
+    echo "Record updated successfully";
+} else {
+    echo "Error updating record: " . $data->error;
+}
+
+
+?>
